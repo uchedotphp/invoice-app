2f8bb004d183b78b8084e2079601f088bad4a430
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-10 17:31:23 +0100

    Integrate sidebar and logo into layout and homepage

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,17 @@
+import Sidebar from "@components/sidebar/Sidebar";
 import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="h-full flex flex-col lg:flex-row">
+      <aside className="overflow-hidden">
+        <Sidebar />
+      </aside>
+      <main className="bg-red-300 flex-1 overflow-y-auto">
+        <Outlet />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
